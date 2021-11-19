868da2755f1d6f66ee202e17603f84d4747da746
sudiptasoumyajit <84839625+sudiptasoumyajit@users.noreply.github.com>
2021-11-19 14:33:26 +0530

    lol lol lol lol lol

---
+++ main.js
@@ -0,0 +1,15 @@
+function setup() {
+    canvas = createCanvas(1000, 400);
+    canvas.center();
+}
+function draw() {}
+function clearCanvas() {
+    background("white");
+    console.log("Debug: The Canvas Has Been Reset Into New Canvas.");
+}
+function view() {
+    console.log("Debug: You Are Viewing The Cheatcodes.");
+    console.log("Debug: You can see the images");
+    console.log("Debug: These are useful and are hand drawn images.");
+    console.log("Debug: This will be useful.");
+}
