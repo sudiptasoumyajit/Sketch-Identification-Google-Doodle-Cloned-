09ea59e4e9a8ecef8975332d6e18a386d7fa9814
sudiptasoumyajit <84839625+sudiptasoumyajit@users.noreply.github.com>
2021-11-20 14:40:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 function setup() {
     canvas = createCanvas(1000, 400);
     canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
 }
-function draw() {}
+function draw() {
+    strokeWeight(13);
+    stroke(0);
+    if (mouseIsPressed) {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+}
 function clearCanvas() {
     background("white");
     console.log("Debug: The Canvas Has Been Reset Into New Canvas.");
@@ -12,4 +21,20 @@ function view() {
     console.log("Debug: You can see the images");
     console.log("Debug: These are useful and are hand drawn images.");
     console.log("Debug: This will be useful.");
+}
+function classifyCanvas() {
+    classifier.classify(canvas, gotResult);
+}
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById('label').innerHTML='Label : '+results[0].label;
+    document.getElementById('confidence').innerHTML='Confidence : '+Math.round(results[0].confidence*100)+' %';
+    utterThis = newSpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
+}
+function preload() {
+    classifier = ml5.imageClassifier("DoodleNet");
 }
